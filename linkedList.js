1c532376236bf73673f73054c65b305f89bc321c
Andrew Ogle <35720141+andrewogle@users.noreply.github.com>
2020-12-06 22:43:44 -0600

    completed linked list

---
+++ linkedList.js
@@ -0,0 +1,66 @@
+class ListNode {
+    constructor( value){
+        this.value = value;
+        this.next = null;
+    }
+
+    getValue(){
+        return this.value;
+    }
+
+    getNext(){
+        return this.next;
+    }
+    setNext(newNext){
+        this.next = newNext;
+    }
+}
+
+class LinkedList{
+    constructor(head = null){
+        this.head = head;
+    }
+
+    addToHead(value){
+        let node = new ListNode(value)
+        if(this.head === null){
+            this.head = node
+        }else{
+            node.next = this.head
+            this.head = node
+        }
+    }
+    contains(value){
+        if(this.head === null){
+            return false
+        }
+        let current = this.head;
+
+        while(current !== null){
+            if (current.getValue() === value){
+                return true
+            }else{
+                current = current.getNext
+            }
+        }
+    }
+    print(){
+        let currentNode = this.head
+        console.log(currentNode.getValue())
+        while(currentNode.next !== null){
+            currentNode = currentNode.next
+            console.log(currentNode.getValue())
+        }
+    }
+}
+
+const list = new LinkedList()
+
+list.addToHead(1)
+list.addToHead(2)
+list.addToHead(3)
+list.addToHead(4)
+list.addToHead(5)
+list.addToHead(6)
+
+list.print()
